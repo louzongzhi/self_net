087b577b5a248a2132a68c645c217f0283f18c72
louzongzhi <2871561809@qq.com>
2024-11-18 10:05:46 +0800

    优化UNet家族模型参数传递方式

--- models/__init__.py
+++ models/__init__.py
@@ -18,13 +18,13 @@ def load_model(model_name, n_channels, num_classes):
     if model_name == "self_net":
         model = self_net(n_channels, num_classes)
     elif model_name == "UNet":
-        model = UNet(n_channels, num_classes)
+        model = UNet(n_channels=n_channels, n_classes=num_classes)
     elif model_name == "UNetplusplus":
-        model = UNetplusplus(n_channels, num_classes)
+        model = UNetplusplus(n_channels, num_classes=num_classes)
     elif model_name == "UNet3plus":
-        model = UNet3plus(n_channels, num_classes)
+        model = UNet3plus(n_channels, n_classes=num_classes)
     elif model_name == "UNetV2":
-        model = UNetV2(n_channels, num_classes)
+        model = UNetV2(n_channels, n_classes=num_classes)
     else:
         raise ValueError("Invalid model name")
 
