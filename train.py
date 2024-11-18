23ef46941fbe7373dee47dd0a78d66fba9a35d63
louzongzhi <2871561809@qq.com>
2024-11-18 11:07:35 +0800

    固定模型保存路径为./model.pth

--- train.py
+++ train.py
@@ -144,7 +144,7 @@ for epoch in range(EPOCHS):
     # Track best performance, and save the model's state
     if avg_vloss < best_vloss:
         best_vloss = avg_vloss
-        model_path = 'model_{}_{}'.format(timestamp, epoch_number)
+        model_path = './model.pth'
         torch.save(model.state_dict(), model_path)
 
     epoch_number += 1
